3cd14e7eba79a7d9d3dc2c5bc0952951510745ad
jbacon11 <81644933+jbacon11@users.noreply.github.com>
2022-07-19 15:04:18 -0500

    Update Chapter 8 Lab Jeremy Bargy.py

--- Chapter 8 Lab Jeremy Bargy.py
+++ Chapter 8 Lab Jeremy Bargy.py
@@ -5,7 +5,7 @@
 ##Write a program which asks for the user's first and last name (in a single prompt).
 ##Then displays the name to the user in proper case. The format for proper case is the first letter of the user's
 ##first name and last name is capitalized and the remaining letters are lower case.
-##For example, the user enters wENdy PAYne the program will display Wendy Payne.
+##For example, the user enters jEreMY bArGy the program will display Jeremy Bargy.
 ##Remember the program will run as many times as the user wishes.
 
 
